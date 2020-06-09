bd5e56e5bb84e92d913b1f560092b1bcb76dab31
Rajesh Mule <rajeshsmule@gmail.com>
2020-06-09 22:55:36 +0530

    add Header

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import "bulma/css/bulma.css";
 import HomePage from "./views/auth/HomePage";
 import { Route, Switch } from "react-router-dom";
+import Header from "./views/common/Header";
 import Login from "./views/auth/login";
 import Signup from "./views/auth/signup";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </>
   );
 }
 
