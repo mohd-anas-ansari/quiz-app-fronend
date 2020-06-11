a5f7f7e16fd3831cf31305b2ea81d863d5ac4357
anasansari157 <anasansari157@gmail.com>
2020-06-11 10:26:31 +0530

    Complete Login And Signup

--- src/state/reducers/currentUser.js
+++ src/state/reducers/currentUser.js
@@ -1,12 +1,25 @@
-var intialState = {};
-function currentUser(state = intialState, action) {
-  switch (action.type) {
-    case "":
-      break;
+var intialState = {
+	userInfo: null,
+	isAuthReqInProgress: false,
+};
 
-    default:
-      return state;
-  }
+function currentUser(state = intialState, action) {
+	switch (action.type) {
+		case "FETCH_CURRENT_USER_START":
+			return {
+				...state,
+				isAuthReqInProgress: true,
+			};
+		case "FETCH_CURRENT_USER_SUCCESS":
+			return {
+				...state,
+				userInfo: action.payload,
+				isAuthReqInProgress: false,
+			};
+
+		default:
+			return state;
+	}
 }
 
 export default currentUser;
